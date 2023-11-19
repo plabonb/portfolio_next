869eed375c0e73809d4f67cf22ab097aeff01bd4
Plabon Biswas <plabonbiswas2050@gmail.com>
2023-11-19 14:58:48 +0600

    AboutMe update

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -2,10 +2,12 @@
 import React from 'react';
 import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
+import AboutSection from './AboutSection';
 
 const HeroSection = () => {
     return (
-        <section>
+        <>
+         <section>
             <div className='grid grid-cols-1 sm:grid-cols-12'>
                 <div className='col-span-7 place-self-center text-center sm:text-left'>
                     <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
@@ -42,7 +44,7 @@ const HeroSection = () => {
                 <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
                     <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
                         <Image
-                            src='/image/pb.png'
+                            src='/images/pb.png'
                             alt='pb'
                             className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
                             width={300}
@@ -52,6 +54,9 @@ const HeroSection = () => {
                 </div>
             </div>
         </section>
+        <AboutSection />
+        </>
+       
     )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
-import Image from 'next/images';
+
 import HeroSection from './components/HeroSection';
 import Navbar from './components/Navbar';
 import AboutSection from './components/AboutSection';
+
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
       <Navbar />
       <div className='container mt-24 mx-auto px-12 py-4'>
         <HeroSection />
-        <AboutSection />
+        {/* <AboutSection /> */}
       </div>
     </main>
   );
