2377fba043474ec489fc90df5a8cf84037f44b09
Plabon Biswas <plabonbiswas2050@gmail.com>
2023-11-23 05:56:06 +0600

    update

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -16,19 +16,19 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+            <span>
               Hello, I&apos;m{" "}
             </span>
             <br></br>
             <TypeAnimation
               sequence={[
-                "Plabon",
+                "Judy",
                 1000,
-                "Font-End Developer",
+                "Web Developer",
                 1000,
                 "Mobile Developer",
                 1000,
-                "Web Developer",
+                "UI/UX Designer",
                 1000,
               ]}
               wrapper="span"
@@ -37,17 +37,18 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-          My name is Plabon Biswas. I&apos;m a front-end developer based in Dhaka, Bangladesh. I have developed many types of front-ends application.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            voluptatum.
           </p>
           <div>
             <Link
-              href="#contact"
+              href="/contact"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
               Hire Me
             </Link>
             <Link
-              href="https://flowcv.com/resume/ts65wvktec"
+              href="/"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
@@ -64,7 +65,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/pb.png"
+              src="/images/hero-image.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
