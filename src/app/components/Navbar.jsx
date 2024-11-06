6fce4ce4b1e764e5ffcddf603c1d8b93a6c8b3a2
Plabon Biswas <88637197+plabonb@users.noreply.github.com>
2024-11-07 03:48:13 +0600

    Update Navbar.jsx

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
                     href={"/"}
                     className="text-2xl md:text-5xl text-lime-500 font-semibold"
                 >
-                    PB<span>..</span>
+                    PB<span className="text-red-600">.</span>
                 </Link>
                 <div className="mobile-menu block md:hidden">
                     {!navbarOpen ? (
